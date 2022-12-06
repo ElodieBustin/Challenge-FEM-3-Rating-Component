e61ff8d5bb8600f57fedb582f4e56d18466f1755
ElodieBustin <elodie.bustin@lilo.org>
2022-12-06 17:11:58 +0100

    enable js script for rating

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+const number = document.querySelectorAll('.rating__numbers--container>li');
+const ratingComponent = document.querySelector('.component__rating');
+const thankYouComponent = document.querySelector('.component__thankYou');
+const spanNumber = document.querySelector('.selected__text__number');
+
+function addHiddenClass(){
+  for(let i = 0; i < number.length; i++){
+    number[i].addEventListener('click', ()=> {
+      ratingComponent.classList.add('hidden');
+      thankYouComponent.classList.remove('hidden');
+      spanNumber.textContent = number[i].textContent;
+      console.log(number[i].textContent);
+    });
+  }
+  
+}
+
+addHiddenClass();
+
